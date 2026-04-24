800f42a5e368a19b52fc36a054a65fdf7c986f11
Khang Nguyen <ronglun412@gmail.com>
2026-04-24 15:32:58 +0700

    [REFACTOR]  Recursion - HaNoiTower

--- src/Recursion/HaNoiTower.py
+++ src/Recursion/HaNoiTower.py
@@ -1,23 +1,22 @@
-def HaNoiTower(n, from_peg, to_peg, aux_peg):
-    print("Call hanoi(n=", n ,", from=", from_peg, ", to=", to_peg, ", aux=", aux_peg, ")")
+class Solution:
+    def HaNoiTower(self, n, start, mid, end):
+        # print(n, start, mid, end)
 
-    if n == 1:
-        print("  -> Move disk 1 from ", from_peg, " to ", to_peg)
-        return
-    
-    # Step 1: move n-1 disks to auxiliary peg
-    HaNoiTower(n - 1, from_peg, aux_peg, to_peg)
+        if n == 1:
+            print("1 move ", start, " to ", end)
+            return
 
-    # Step 2: move the largest disk
-    print("  -> Move disk ", n, " from ", from_peg, " to ", to_peg)
+        self.HaNoiTower(n - 1, start, end, mid)
 
-    # Step 3: move n-1 disks to destination peg
-    HaNoiTower(n - 1, aux_peg, to_peg, from_peg)
+        print(n, "move ", start, " to ", end)
+
+        self.HaNoiTower(n - 1, mid, start, end)
+
 
 def main():
-    n = 3
-    print("Hanoi Tower with ", n, " disks:")
-    HaNoiTower(n, 'A', 'C', 'B')
+    sol = Solution()
+    print("result: ", sol.HaNoiTower(3, "A", "B", "C"))
+
 
 if __name__ == "__main__":
     main()
