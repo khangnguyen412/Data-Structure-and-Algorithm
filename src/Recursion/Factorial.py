05f5316314d8cf7ffab4bc4f9b40047e93bfad73
Khang Nguyen <ronglun412@gmail.com>
2026-05-01 15:26:41 +0700

    [REFACTOR] Reacfactor - Factorial

--- src/Recursion/Factorial.py
+++ src/Recursion/Factorial.py
@@ -1,12 +1,15 @@
-def factorial(n):
-    if (n == 0):
-        return 1
-    else:
-        return n * factorial(n - 1)
-    
+class Solution:
+    def Factorial(self, n):
+        if n == 0:
+            return 1
+        else:
+            return n * self.Factorial(n - 1)
+
+
 def main():
-    n = 5
-    print("Factorial of ", n, " is: ", factorial(n))
+    sol = Solution()
+    print("result: ", sol.Factorial(5))
+
 
 if __name__ == "__main__":
     main()
