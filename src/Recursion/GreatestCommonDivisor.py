0851812390118fe52e46f21a44a7e1002797023c
Khang Nguyen <ronglun412@gmail.com>
2026-05-03 14:24:54 +0700

    [REFACTOR] Reacfactor - Greatest Common Divisor

--- src/Recursion/GreatestCommonDivisor.py
+++ src/Recursion/GreatestCommonDivisor.py
@@ -1,14 +1,15 @@
-def GDC(p, q):
-    r = p % q
-    if r == 0:
-        return q
-    else:
-        return GDC(q, r)
-    
+class Solution:
+    def GreatestCommonDivisor(self, p, q):
+        if p % q == 0:
+            return q
+        else:
+            return self.GreatestCommonDivisor(q, p % q)
+
+
 def main():
-    p = 18
-    q = 48
-    print("GDC(", p, ",", q, ") = ", GDC(p, q))
+    sol = Solution()
+    print("result: ", sol.GreatestCommonDivisor(4, 7))
+
 
 if __name__ == "__main__":
     main()
